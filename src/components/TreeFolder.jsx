faabee1d722bef00a30721fe77ba914c3d89fa4b
rafi <r.rafi1615005@gmail.com>
2023-07-10 05:46:00 +0600

    row-issue-solved

--- src/components/TreeFolder.jsx
+++ src/components/TreeFolder.jsx
@@ -5,6 +5,7 @@ export const TreeFolder = (props) => {
     const {name, children,  id} = props
     const [isExpanded, setIsExpanded] = useState(false)
   return (
+  <>
     <div data-folderId={id} className='border h-12 border-solid rounded flex items-center justify-start my-3 relative'>
         <div className='h-full w-10 bg-slate-300 flex justify-center items-center'>
         <div className='h-1/2 w-1/2 bg-white rounded'></div>
@@ -16,7 +17,8 @@ export const TreeFolder = (props) => {
         <p className='p-1 px-2 bg-slate-300 h-1/2 flex items-center rounded justify-center cursor-pointer' onClick={() => setIsExpanded(false)}>{'v'}</p>
         }
         {/* children && isExpanded && children.map((c) => <SingleBar key={c.name} data={c.name} className='m-4'/>) */}
-        {children &&isExpanded  && <div className={`m-4 absolute top-6 w-10/12 z-20 `}>{children}</div>}
-    </div>
+     </div>
+    {children &&isExpanded  && <div className={`m-4 top-6 w-10/12 z-20 `}>{children}</div>}
+  </>
   )
 }
